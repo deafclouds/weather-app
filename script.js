9579ff0dd6c3b1bb3c10ddd237f527d6dbe23398
Andrés <agch.rivas@protonmail.com>
2021-08-07 19:39:01 -0500

    totalintegration

---
+++ script.js
@@ -0,0 +1,42 @@
+let weather = {
+    apiKey: "989497e253921c6dee9800308046ea88",
+    fetchWeather: function(city) {
+        fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city
+        + "&units=metric&appid="
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity, feels_like } = data.main;
+        const { speed } = data.wind;
+        // console.log(name, icon, description, humidity, speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".feels").innerText = "Feels like " + feels_like + "°C";
+        document.querySelector(".icon").src ="https://openweathermap.org/img/wn/"+ icon +".png";
+        document.querySelector(".desc").innerText = description;
+        document.querySelector(".hum").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerHTML = "Wind Speed: " + speed + " Km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
+
+document.querySelector(".searchme").addEventListener('click', function() {
+    weather.search();
+})
+document.querySelector(".search-bar").addEventListener("keyup", function(Event) {
+    if (Event.key == "Enter") {
+        weather.search();
+    }
+})
+weather.fetchWeather("Hong Kong")
