cfb357c231f0c3e7d03aab7e333367bc83694c24
Andrés <agch.rivas@protonmail.com>
2021-08-07 20:49:07 -0500

    jhjh

--- script.js
+++ script.js
@@ -13,19 +13,20 @@ let weather = {
     displayWeather: function(data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity, feels_like } = data.main;
+        const { temp, humidity, feels_like, pressure } = data.main;
         const { speed } = data.wind;
         const { country } = data.sys;
-        const { timezone } = data.timezone;
-        document.querySelector(".city").innerText = "Weather in " + name;
+        // const { timezone } = data.sys;
+        document.querySelector(".city").innerText = name;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".feels").innerText = "Feels like " + feels_like + "°C";
         document.querySelector(".icon").src ="https://openweathermap.org/img/wn/"+ icon +".png";
         document.querySelector(".desc").innerText = description;
         document.querySelector(".hum").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".pres").innerHTML = "Pressure: " + pressure;
         document.querySelector(".wind").innerHTML = "Wind Speed: " + speed + " Km/h";
         document.querySelector(".count").innerHTML = "Country: " + country;
-        document.querySelector(".timezone").innerHTML = "Timezone: " + timezone;
+        // document.querySelector(".timezone").innerHTML = "Timezone: " + timezone;
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')"
     },
